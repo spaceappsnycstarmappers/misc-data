43d0aa9dfde1651577da4075cedaf8b0026e6331
Robert Carlsen <robert@robertcarlsen.net>
2013-04-21 12:31:10 -0400

    making a function for the hood
    accepts a list of 3-tuple data

--- cloud-gen.py
+++ cloud-gen.py
@@ -14,12 +14,14 @@ import scipy
 from scipy import spatial
 
 # make some fake:
-mock_stars = []
-for i in range(0, 20, 1):
-    this_star = []
-    for c in range(3):
-        this_star.append(random())
-    mock_stars.append(this_star)
+def mock_data():
+    mock_stars = []
+    for i in range(0, 20, 1):
+        this_star = []
+        for c in range(3):
+            this_star.append(random())
+        mock_stars.append(this_star)
+    return mock_stars
 
 #print mock_stars
 
@@ -32,10 +34,11 @@ model_data = []
 # lets presume this is mm.
 scale = 50
 
+"""
+mock_stars = []
 star_base_radius = 3.0
 column_radius = [2.0, 1.0]
 
-# this is the naive 'ball and stick' representation:
 for i, coord in enumerate(mock_stars):
     #print coord
     scaled_coord = [x*scale for x in coord]
@@ -54,30 +57,6 @@ for i, coord in enumerate(mock_stars):
 # combine all model_data and add a base plate, 
 u = translate([-scale/2, -scale/2, 0])(model_data,translate([-5,-5,0])(cube([scale+10, scale+10, 2])))
 
-"""
-# trying a more sophisticated model
-# k-nearest neighbors
-# inverting space...filling the "space" with shape
-neighbors_list = []
-# play with the leafsize here
-star_tree = scipy.spatial.cKDTree(mock_stars,leafsize=100)
-for star in mock_stars:
-    # need to twiddle these dials
-    neighbors = star_tree.query(star,k=2,distance_upper_bound=20)
-    # print neighbors
-    
-    scaled_coord = [x*scale for x in mock_stars[neighbors[1][0]]]
-    # TODO: make a column between the current node and neighbor
-    neighbors_list.append(
-            # getting our location, and the distance as the sphere
-            translate(scaled_coord)(sphere(scale*neighbors[0][1]))
-            )
-model_data = neighbors_list
-
-# no need for a base plate here (but may need support)
-u = translate([-scale/2, -scale/2, 0])(model_data)
-"""
-
 scad = scad_render(u)
 #print scad
 
@@ -87,11 +66,69 @@ scad = "$fn=20;\n"+scad
 f = open('output.scad', 'w')
 f.write(scad)
 f.close()
+"""
+
+# trying a more sophisticated model
+# k-nearest neighbors
+# inverting space...filling the "space" with shape
+def hood(star_data):
+    mock_stars = star_data
+    neighbors_list = []
+    minimum_height = 10000 # used to calculate a base plane
+    g = 1.01 # fudge factor to ensure intersections with each sphere
+
+    # play with the leafsize here
+    star_tree = scipy.spatial.cKDTree(mock_stars,leafsize=100)
+    for star in mock_stars:
+        # need to twiddle these dials
+        neighbors = star_tree.query(star,k=2,distance_upper_bound=20)
+        # print neighbors
+        
+        scaled_coord = [x*scale for x in mock_stars[neighbors[1][0]]]
+        scaled_radius = g*scale*neighbors[0][1]
+        # TODO: make a column between the current node and neighbor
+        neighbors_list.append(
+                # getting our location, and the distance as the sphere
+                translate(scaled_coord)(sphere(scaled_radius))
+                )
+        # update the minimum_height if necessary
+        bottom = scaled_coord[2] - scaled_radius
+        if bottom < minimum_height:
+            minimum_height = bottom
+
+    model_data = neighbors_list
+
+    # shift the data to the center, and up a bit to make way for the base
+    u = translate([-scale/2, -scale/2,
+        (-1 * minimum_height if minimum_height < 0 else 0) +5])(model_data)
+
+    # make a base pedestal
+    u += cylinder(h=40, r=7) + cylinder(h=2, r=20)
+    return u
+
+
+# generate one 'hood
+u = hood(mock_data())
+scad = scad_render(u)
+f = open('output.scad', 'w')
+f.write("$fn=30;\n"+scad)
+f.close()
+
+"""
+# make several iterations
+for n in range(0, 10):
+    u = hood(mock_data())
+    scad = scad_render(u)
+
+    f = open('hood-'+str(n)+'.scad', 'w')
+    f.write(scad)
+    f.close()
+"""
 
 # writing directly to a file:
 #scad_render_to_file(u, 'output.scad')
 
-
+"""
 # and now for something completely different:
 # try the .xyz format
 # item count
@@ -106,5 +143,5 @@ for star in range(0, len(mock_stars)):
 ff = open('output.xyz', 'w')
 ff.write(xyz)
 ff.close()
-
+"""
 
